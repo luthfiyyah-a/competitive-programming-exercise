0db734e3629469ab99f214779e44f5a1309a91e3
luthfiyyah hanifah amari <luthfiyyah.205025@gmail.com>
2024-03-02 00:00:30 +0700

    rename file 2769. find the maximum achievable number.cpp

---
+++ leetcode/2769. find the maximum achievable number.cpp
@@ -0,0 +1,11 @@
+// 1 mar 2024
+// by: luthfiyyah hanifah amari
+// level: easy
+// https://leetcode.com/problems/find-the-maximum-achievable-number/
+
+class Solution {
+public:
+    int theMaximumAchievableX(int num, int t) {
+        return num + 2*t;
+    }
+};
